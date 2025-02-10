d750a3a5c1e8debe89a902f81e6b308078450859
Carlos Eduardo <ducameneses16@gmail.com>
2025-02-09 23:35:41 -0300

    feat: Create health check endpoint

---
+++ green_bank/api/controllers/health_check.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+
+health_check = Blueprint('health_check', __name__, url_prefix='/')
+
+
+@health_check.route('', methods=['GET'])
+def verify_health_check():
+    return {'status': 'ok'}
