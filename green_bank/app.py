d1b89de96ea66e9869d95d0f22c9ad6a59ee0d26
Carlos Eduardo <ducameneses16@gmail.com>
2025-02-10 03:27:22 -0300

    feat: Add configuration for flask jwt extended

--- green_bank/app.py
+++ green_bank/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 
 from green_bank.api import routers
+from green_bank.application.errors.green_bank_exception import GreenBankBasicException
+from green_bank.application.errors.handler.error_handling import error_handling
+from green_bank.infra.settings import Settings
 
 
 app = Flask(__name__)
+jwt = JWTManager(app)
 
-
+app.secret_key = Settings().SECRET_KEY
+app.config['JWT_SECRET_KEY'] = Settings().JWT_SECRET_KEY
+app.register_error_handler(GreenBankBasicException, error_handling)
 app.register_blueprint(routers.api_router)
--- green_bank/infra/settings.py
+++ green_bank/infra/settings.py
@@ -7,5 +7,6 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str
     SECRET_KEY: str
+    JWT_SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
