fba3869db0e4e75badb9373862ea1944ad837ad8
Carlos Eduardo <ducameneses16@gmail.com>
2025-02-10 03:46:13 -0300

    feat: Create email validation

---
+++ green_bank/application/validators/email_validate.py
@@ -0,0 +1,9 @@
+import re
+
+
+def email_validate(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if re.match(pattern, email):
+        return True
+    
+    return False
