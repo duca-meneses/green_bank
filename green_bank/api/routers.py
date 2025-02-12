822e8b4027301e36ff9993627eace152111ebbb1
Carlos Eduardo <ducameneses16@gmail.com>
2025-02-12 04:10:36 -0300

    style: Modifies the import style for relative imports in this file

--- green_bank/api/routers.py
+++ green_bank/api/routers.py
@@ -1,8 +1,8 @@
 from flask.blueprints import Blueprint
 
-from green_bank.api.controllers.auth_controller import app as auth_controller
-from green_bank.api.controllers.health_check import health_check
-from green_bank.api.controllers.user_controller import app as user_controller
+from .controllers.auth_controller import bp as auth_controller
+from .controllers.health_check import health_check
+from .controllers.user_controller import app as user_controller
 
 api_router = Blueprint('api', __name__)
 api_router.register_blueprint(health_check)
