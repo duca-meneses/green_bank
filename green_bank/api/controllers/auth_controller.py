ebf6b202419813a126510227f1668c07c527286e
Carlos Eduardo <ducameneses16@gmail.com>
2025-02-12 04:08:17 -0300

    feat: Add validations for user and auth controller using the schemas
    - Adds the endpoint for updating the user's password inside the auth controller

--- green_bank/api/controllers/auth_controller.py
+++ green_bank/api/controllers/auth_controller.py
@@ -1,17 +1,43 @@
 from http import HTTPStatus
 
 from flask import Blueprint, request
+from marshmallow import ValidationError
 
+from green_bank.application.errors.green_bank_exception import GreenBankBasicException
+from green_bank.application.schemas.auth_schema import (
+    CreateLoginSchema,
+    PasswordChangeSchema,
+)
 from green_bank.application.services.auth_service import auth_service
 
-app = Blueprint('auth', __name__, url_prefix='/api/auth')
+bp = Blueprint('auth', __name__, url_prefix='/api/auth')
 
-@app.route("/login", methods=["POST"])
+@bp.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("email", None)
-    password = request.json.get("password", None)
+    login_schema = CreateLoginSchema()
+    try:
+        data = login_schema.load(request.json)
+    except ValidationError as error:
+        raise GreenBankBasicException(error.messages, HTTPStatus.UNPROCESSABLE_ENTITY)
+    access_token = auth_service.login(data['email'], data['password'])
 
-    access_token = auth_service.login(username, password)
+    return access_token, HTTPStatus.OK
 
 
-    return {'access_token': access_token}, HTTPStatus.OK
+@bp.route("/<uuid:user_id>/change-password", methods=["Patch"])
+def change_password(user_id):
+    password_schema = PasswordChangeSchema()
+    try:
+        data = password_schema.load(request.json)
+
+        if data['new_password'] != data['new_password_confirm']:
+            raise GreenBankBasicException(
+                "Passwords do not match",
+                HTTPStatus.UNPROCESSABLE_ENTITY
+            )
+    except ValidationError as error:
+        raise GreenBankBasicException(error.messages, HTTPStatus.UNPROCESSABLE_ENTITY)
+
+    auth_service.change_password(user_id, data)
+
+    return {'message': 'Password changed successfully'}, HTTPStatus.OK
--- green_bank/api/controllers/user_controller.py
+++ green_bank/api/controllers/user_controller.py
@@ -2,7 +2,13 @@ from http import HTTPStatus
 
 from flask import request
 from flask.blueprints import Blueprint
+from marshmallow import ValidationError
 
+from green_bank.application.errors.green_bank_exception import GreenBankBasicException
+from green_bank.application.schemas.user_schema import (
+    CreateUpdateUserSchema,
+    CreateUserSchema,
+)
 from green_bank.application.services.user_service import user_service
 
 app = Blueprint('user', __name__, url_prefix='/api/users')
@@ -10,64 +16,30 @@ app = Blueprint('user', __name__, url_prefix='/api/users')
 
 @app.route('/', methods=['POST'])
 def create_user():
-    data = request.json
+    user_schema = CreateUserSchema()
+    try:
+        data = user_schema.load(request.json) # type: ignore
+    except ValidationError as error:
+        raise GreenBankBasicException(error.messages, HTTPStatus.UNPROCESSABLE_ENTITY)
     user = user_service.create_user(user=data)
-    response = {
-        "id": user.id,
-        "name": user.name,
-        "email": user.email,
-        "user_type": user.user_type,
-        "document_number": user.document_number,
-        "wallet_balance": user.wallet_balance,
-        "created": user.created,
-    }
-    return response, HTTPStatus.CREATED
+    return user, HTTPStatus.CREATED
 
 @app.route('/', methods=['GET'])
 def list_users():
     users = user_service.get_users()
-    response = []
-    if len(users) == 0:
-        return response, HTTPStatus.OK
-
-    for user in users:
-        response.append({
-            "id": user.id,
-            "name": user.name,
-            "email": user.email,
-            "user_type": user.user_type,
-            "document_number": user.document_number,
-            "wallet_balance": user.wallet_balance,
-            "created": user.created,
-        })
-    return {'users': response}, HTTPStatus.OK
+    return {'users': users}, HTTPStatus.OK
 
 @app.route('/<uuid:user_id>', methods=['GET'])
 def get_user(user_id):
     user = user_service.get_user(user_id)
-    response = {
-        "id": user.id,
-        "name": user.name,
-        "email": user.email,
-        "user_type": user.user_type,
-        "document_number": user.document_number,
-        "wallet_balance": user.wallet_balance,
-        "created": user.created,
-    }
-    return response, HTTPStatus.OK
+    return user, HTTPStatus.OK
 
 @app.route('/<uuid:user_id>', methods=['PUT'])
 def update_user(user_id):
-    data = request.json
+    user_schema = CreateUpdateUserSchema()
+    data = user_schema.load(request.json) # type: ignore
     user = user_service.update_user(user_id, user=data)
-    response = {
-        "id": user.id,
-        "name": user.name,
-        "email": user.email,
-        "created": user.created,
-        "updated": user.updated,
-    }
-    return response, HTTPStatus.OK
+    return user, HTTPStatus.OK
 
 @app.route('/<uuid:user_id>', methods=['DELETE'])
 def delete_user(user_id):
