ee219f46057d0aa6029fad2eb6bbb86ad54a9009
Carlos Eduardo <ducameneses16@gmail.com>
2025-02-22 00:37:27 -0300

    feat: Add typing in variable errors

--- green_bank/application/errors/handler/error_handling.py
+++ green_bank/application/errors/handler/error_handling.py
@@ -5,5 +5,5 @@ from green_bank.application.errors.green_bank_exception import GreenBankBasicExc
 app = Blueprint('error_handling', __name__)
 
 @app.errorhandler(GreenBankBasicException)
-def error_handling(error):
+def error_handling(error: GreenBankBasicException):
     return error.to_dict(), error.status_code
